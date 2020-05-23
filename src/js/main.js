2daf6b1c6a99ac0b4d3778202a60b2b0fa824497
Chukwudum Chukwuedo <kriz2phorus@gmail.com>
2020-05-23 03:45:03 +0100

    fix: second pass on codacy reviews for pull request

--- src/js/main.js
+++ src/js/main.js
@@ -3,36 +3,36 @@
   /*
 	[ Validate ]
 	*/
-	function validate(input) {
-		if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
-			if (
-				$(input)
-					.val()
-					.trim()
-					.match(
-						/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
-					) == null
-			) {
-				return false;
-			}
-		} else {
-			if ($(input).val().trim() == '') {
-				return false;
-			}
-		}
-	}
+    function validate(input) {
+      if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+        if (
+          $(input)
+            .val()
+            .trim()
+            .match(
+              /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+            ) == null
+        ) {
+          return false;
+        }
+      } else {
+        if ($(input).val().trim() == '') {
+          return false;
+        }
+      }
+    }
 
-	function showValidate(input) {
-		var thisAlert = $(input).parent();
+    function showValidate(input) {
+      var thisAlert = $(input).parent();
 
-		$(thisAlert).addClass('alert-validate');
-	}
+      $(thisAlert).addClass('alert-validate');
+    }
 
-	function hideValidate(input) {
-		var thisAlert = $(input).parent();
+    function hideValidate(input) {
+      var thisAlert = $(input).parent();
 
-		$(thisAlert).removeClass('alert-validate');
-	}  
+      $(thisAlert).removeClass('alert-validate');
+    }  
     
   var input = $('.validate-input .input100');
 
@@ -54,8 +54,9 @@
       hideValidate(this);
     });
   });
-  /*==================================================================
-    [ Show pass ]*/
+  /*
+    [ Show pass ]
+  */
   var showPass = 0;
   $('.btn-show-pass').on('click', function () {
     if (showPass === 0) {
