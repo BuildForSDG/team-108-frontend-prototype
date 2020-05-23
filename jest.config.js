0384312982c271a36218c81e8d578e2e13303107
Chukwudum Chukwuedo <kriz2phorus@gmail.com>
2020-05-23 03:26:04 +0100

    fix: first pass to fix codacy errors to facilitate pull request

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-export const moduleFileExtensions = ['js', 'json'];
-export const rootDir = '__tests__';
-export const testRegex = ['.spec.js$', '.test.js$'];
-export const coverageDirectory = './coverage';
-export const testEnvironment = 'node';
+module.exports = {
+  moduleFileExtensions: ['js', 'json'],
+  rootDir: '__tests__',
+  testRegex: ['.spec.js$', '.test.js$'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+};
--- loginScreen/js/main.js
+++ loginScreen/js/main.js
@@ -1,15 +1,46 @@
 (function ($) {
-  'use strict';
+  
+  /*
+	[ Validate ]
+	*/
+	function validate(input) {
+		if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+			if (
+				$(input)
+					.val()
+					.trim()
+					.match(
+						/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+					) == null
+			) {
+				return false;
+			}
+		} else {
+			if ($(input).val().trim() == '') {
+				return false;
+			}
+		}
+	}
 
-  /*==================================================================
-    [ Validate ]*/
+	function showValidate(input) {
+		var thisAlert = $(input).parent();
+
+		$(thisAlert).addClass('alert-validate');
+	}
+
+	function hideValidate(input) {
+		var thisAlert = $(input).parent();
+
+		$(thisAlert).removeClass('alert-validate');
+	}  
+    
   var input = $('.validate-input .input100');
 
   $('.validate-form').on('submit', function () {
     var check = true;
 
-    for (var i = 0; i < input.length; i++) {
-      if (validate(input[i]) == false) {
+    for (var i = 0; i < input.length; i += 1) {
+      if (validate(input[i]) === false) {
         showValidate(input[i]);
         check = false;
       }
@@ -23,42 +54,11 @@
       hideValidate(this);
     });
   });
-
-  function validate(input) {
-    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-      if (
-        $(input)
-          .val()
-          .trim()
-          .match(
-            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
-          ) == null
-      ) {
-        return false;
-      }
-    } else {
-      if ($(input).val().trim() == '') {
-        return false;
-      }
-    }
-  }
-
-  function showValidate(input) {
-    var thisAlert = $(input).parent();
-
-    $(thisAlert).addClass('alert-validate');
-  }
-
-  function hideValidate(input) {
-    var thisAlert = $(input).parent();
-
-    $(thisAlert).removeClass('alert-validate');
-  }
   /*==================================================================
     [ Show pass ]*/
   var showPass = 0;
   $('.btn-show-pass').on('click', function () {
-    if (showPass == 0) {
+    if (showPass === 0) {
       $(this).next('input').attr('type', 'text');
       $(this).find('i').removeClass('fa-eye');
       $(this).find('i').addClass('fa-eye-slash');
